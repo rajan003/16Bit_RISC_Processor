c49e45f8923176773613be8a3722c32f254bb610
Anurag Singh <anuragsi003@gmail.com>
2026-01-03 13:08:08 +0100

    Refactor cpu_pkg.sv with packed structs and parameters
    Refactor CPU package to include packed structures and parameters.

--- cpu_pkg.sv
+++ cpu_pkg.sv
@@ -1,24 +1,48 @@
-package cpu_pkg;
+`ifndef CPU_PKG_SV
+`define CPU_PKG_SV
+// --------------------
+// Global parameters
+// --------------------
+parameter  INST_ADDR_WIDTH = 12;
+parameter  INST_DATA_WIDTH = 32;
 
-    typedef struct {
-                  logic isAdd;
-                  logic isSub;
-                  logic isCmp;
-                  logic isMul;
-                  logic isDiv;
-                  logic isMod;
-                  logic isLsl;
-                  logic isLsr;
-                  logic isAsr;
-                  logic isOr;
-                  logic isAnd;
-                  logic isNot;
-                  logic isMov } aluctrl ;
+parameter MEM_ADDR_WIDTH = 12;
+parameter MEM_DATA_WIDTH = 32;
 
-typedef struct {
-                     logic GT ;
-                     logic ET ;
-   } flg;
-parameter INST_ADDR_WIDTH =32;
-parameter INST_DATA_WIDTH= 12;
-endpackage
+// If you want DMEM params too:
+//parameter int unsigned DMEM_ADDR_WIDTH = 12;   // 4K words
+//parameter int unsigned DMEM_DATA_WIDTH = 32;
+
+// --------------------
+// Common types
+// --------------------
+typedef struct packed {
+  logic isAdd;
+  logic isSub;
+  logic isCmp;
+  logic isMul;
+  logic isDiv;
+  logic isMod;
+  logic isLsl;
+  logic isLsr;
+  logic isAsr;
+  logic isOr;
+  logic isAnd;
+  logic isNot;
+  logic isMov;
+} aluctrl_t;
+
+typedef struct packed {
+  logic GT;
+  logic ET;
+} flag_t;
+
+typedef enum logic [1:0] {
+  ALU_AND = 2'b00,
+  ALU_OR  = 2'b01,
+  ALU_NOT = 2'b10
+} logic_op_t;  // For logical Calculation in ALU
+
+
+
+`endif
